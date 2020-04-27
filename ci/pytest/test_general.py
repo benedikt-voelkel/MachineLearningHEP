ea7d68397d4ed5cd758884925801b1e382db1502
Benedikt Volkel <benedikt.volkel@cern.ch>
2020-04-27 22:44:49 +0200

    Back to dict

--- ci/pytest/test_general.py
+++ ci/pytest/test_general.py
@@ -3,4 +3,4 @@ from machine_learning_hep.io import parse_yaml
 YAML_PATH="ci/pytest/yaml.yaml"
 
 def test_yaml():
-    assert isinstance(parse_yaml(YAML_PATH), int)
+    assert isinstance(parse_yaml(YAML_PATH), dict)
